8f23179d6447c92550b2a98f3ddc0247b4bb97c1
Nathan Pham <nathanpham.me@gmail.com>
2023-06-05 21:12:13 -0700

    [:repeat:] Update build

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     input: "./src/index.ts",
     output: {
         dir: "dist",
-        format: "cjs",
+        format: "esm",
     },
     plugins: [typescript(), terser()],
 });
